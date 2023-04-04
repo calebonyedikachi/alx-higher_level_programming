9e4f8d8283c426c084a587dd10f6cafdfe6c595e
calebonyedikachi <117799243+calebonyedikachi@users.noreply.github.com>
2023-04-04 21:18:57 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Code that defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Code Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """For Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -16,7 +16,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """Code for Getting/setting the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """Code for Getting/setting the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -41,18 +41,18 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """Code for returning the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
+        """Code for returning the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
-        Represents the rectangle with the # character.
+        """Code for returning the printable representation of the Rectangle.
+        This represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
